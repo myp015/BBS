992e9468bd4da8e9f50d14dc50095c14fbb48ab4
myp015 <myp015@163.com>
2013-04-16 09:13:13 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BBS::Application.config.secret_token = '8b3d48a9b8b1490788187d28b677473a6fbe8accec220cb31809bcffa1e094d2c194b6468110fd9abfedbbf9d24678acc541265bc90fe592bb331015ca1db3a0'
